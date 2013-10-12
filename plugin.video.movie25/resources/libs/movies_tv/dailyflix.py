0a440b75eb209cbc400fc97790b9df484c092450
kubakul <kubakul@live.com>
2013-10-12 19:51:49 +0200

    Improve imports in dailyflix

--- plugin.video.movie25/resources/libs/movies_tv/dailyflix.py
+++ plugin.video.movie25/resources/libs/movies_tv/dailyflix.py
@@ -1,17 +1,12 @@
-import urllib,urllib2,re,cookielib,urlresolver,os,sys
-import xbmc, xbmcgui, xbmcaddon, xbmcplugin
+import urllib,re,os,sys
+import xbmc,xbmcgui,xbmcaddon,xbmcplugin
 from resources.libs import main
 
 #Mash Up - by Mash2k3 2012.
 
-from t0mm0.common.addon import Addon
-from universal import playbackengine, watchhistory
 addon_id = 'plugin.video.movie25'
 selfAddon = xbmcaddon.Addon(id=addon_id)
-addon = Addon('plugin.video.movie25', sys.argv)
 art = main.art
-    
-wh = watchhistory.WatchHistory('plugin.video.movie25')
 
 def LISTSP3(murl):
         if murl == 'HD':
@@ -44,6 +39,7 @@ def LINKSP3(mname,url):
         main.GA("Dailyfix","Watched")
         sources = []
         ok=True
+        xbmc.executebuiltin("XBMC.Notification(Please Wait!,Collecting Hosts,8000)")
         infoLabels =main.GETMETAT(mname,'','','')
         video_type='movie'
         season=''
@@ -52,14 +48,15 @@ def LINKSP3(mname,url):
         fanart =infoLabels['backdrop_url']
         imdb_id=infoLabels['imdb_id']
         infolabels = { 'supports_meta' : 'true', 'video_type':video_type, 'name':str(infoLabels['title']), 'imdb_id':str(infoLabels['imdb_id']), 'season':str(season), 'episode':str(episode), 'year':str(infoLabels['year']) }
-        xbmc.executebuiltin("XBMC.Notification(Please Wait!,Collecting Hosts,3000)")
         link=main.OPENURL(url)
         match=re.compile("<a href='(.+?)' class='.+?' title='.+?' rel='.+?'>.+?</a").findall(link)
+        import urlresolver
+        main.CloseAllDialogs()
         for murl in match:
                 host=re.compile("http://(.+?).com/.+?").findall(murl)
                 for hname in host:
-                        hname=hname.replace('www.','')
-                        hosted_media = urlresolver.HostedMediaFile(url=murl, title=hname)
+                        hname=hname.replace('www.','').title()
+                        hosted_media = urlresolver.HostedMediaFile(url=murl, title="[COLOR blue]" + hname + "[/COLOR]")
                         sources.append(hosted_media)
         if (len(sources)==0):
                 xbmc.executebuiltin("XBMC.Notification(Sorry!,Movie doesn't have playable links,5000)")
@@ -74,9 +71,12 @@ def LINKSP3(mname,url):
                         return
                 infoL={'Title': infoLabels['title'], 'Plot': infoLabels['plot'], 'Genre': infoLabels['genre']}
                 # play with bookmark
+                from universal import playbackengine
                 player = playbackengine.PlayWithoutQueueSupport(resolved_url=stream_url, addon_id=addon_id, video_type=video_type, title=str(infoLabels['title']),season=str(season), episode=str(episode), year=str(infoLabels['year']),img=img,infolabels=infoL, watchedCallbackwithParams=main.WatchedCallbackwithParams,imdb_id=imdb_id)
                 #WatchHistory
                 if selfAddon.getSetting("whistory") == "true":
+                    from universal import  watchhistory
+                    wh = watchhistory.WatchHistory('plugin.video.movie25')
                     wh.add_item(mname+' '+'[COLOR green]DailyFix[/COLOR]', sys.argv[0]+sys.argv[2], infolabels=infolabels, img=img, fanart=fanart, is_folder=False)
                 player.KeepAlive()
                 return ok
